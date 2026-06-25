1d33bc271a5d044afd232d034886de6e34c11384
Sevin H <sevinh@Mac.fritz.box>
2026-06-25 22:16:35 +0200

    feat: add Fabric 1.21.1 mod template

---
+++ template/src/main/java/com/factory/mod/ModMain.java
@@ -0,0 +1,14 @@
+package com.factory.mod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ModMain implements ModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("mod-factory");
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Mod initialized!");
+    }
+}
